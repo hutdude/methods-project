7ed7d7cf0b8d408d3128fd8dbd4044639825a66f
hutdude <100716056+hutdude@users.noreply.github.com>
2022-04-28 12:25:51 -0500

    Current iteration of main

--- main.py
+++ main.py
@@ -30,9 +30,13 @@ def preLoginLoop():
             if found == True:
                 print("Welcome " + username)
                 return (True, username)
+            if found == False:
+                print("Invalid credentials, try again...")
+                preLoginLoop()
             else:
                 print("Error logging in... Make sure your credentials are corrrect.\n")
                 print("If you do not have an account enter 3 to make one now.")
+                preLoginLoop()
         elif choice == 2:
             while(True):
                 newUsername = input("Enter your desired username: ")
@@ -75,13 +79,29 @@ def postLoginLoop(username):
     try:
         choice = int(input("Select One of the following:\n1. View Books\n2. View Cart\n3. View Profile\n4. Logout\n5. Exit Program"))
         if choice == 1:
-            # display book list with info, then three options: add book to cart, view cart, return to menu
-            
-            ### display books here
+            # displays books, author, and price
+            i = 0
+            while(i < len(bookList)):
+                print("Item " + (i + 1) + ". ", bookList[i].getTitle() + " by " + bookList[i].getAuthor() + " - " + bookList[i].getPrice())
+                i += i
+                
+            # then three options: add book to cart, view cart, return to menu
             
             opt = int(input("Select an option:\n1. Add item to Cart\n2. View Cart\n3. Return to Menu"))
             if opt == 1:
-                
+                while(1):
+                    try:
+                        bookChoice = int(input("Which book would you like to add to your cart? (Enter item number..."))
+                        qtyChoice = int(input("How many would you like to add?"))
+                        # check to make sure valid input
+                        isbnChoice = bookList[bookChoice - 1].getISBN()
+                        priceChoice = bookList[bookChoice - 1].getPrice()
+                        yourCart.addItem(isbnChoice, qtyChoice, priceChoice)
+                        ourInv.EditQty(isbnChoice, )                       
+                            
+                        break
+                    except:
+                        ("An error occured, please try again")
             elif opt == 2:
                 viewCart()
             elif opt == 3:
@@ -91,6 +111,7 @@ def postLoginLoop(username):
             viewCart()
         elif choice == 3:
             # display basic info like name and user name, then four options: Edit info, view order history, delete account, return to menu
+            print("username: " + yourUser.getUsername())
         elif choice == 4:
             preLoginLoop(username)
         elif choice == 5:
@@ -104,10 +125,17 @@ def postLoginLoop(username):
 
 # loop through cart books and display info
 def viewCart():
-    ISBNs = Cart.getISBNs()
-    qtys = Cart.getQTYs()
+    ISBNs = yourCart.getISBNs()
+    qtys = yourCart.getQTYs()
     
     ## loop through inventory to get appropraite info, then print it all out
+    x = 0
+    while x < len(ISBNs):
+        for y in bookList:
+            if ISBNs[x] == y.getISBN():
+                print(qtys[x] + "copies of " + y.getTitle() + " by " + y.getAuthor())
+        x += x
+    print("Current Total: $" + yourCart.getCurrentTotal())
     
     num = int(input("1. Remove Item(s)\n2. Checkout\n3. Return to Menu"))
     
@@ -115,6 +143,7 @@ def viewCart():
         try:
             isbnDelInd = int(input("Which item would you like to remove?"))
             qtyDelNum = int(input("How many would you like to remove?"))
+            yourCart.removeItem(isbnDelInd, qtyDelNum, )
             
 
         except:
@@ -129,12 +158,24 @@ def viewCart():
         viewCart()
         
 def checkout():
-    
+    print("Your total is: $"  +  yourCart.getCurrentTotal()+ "\n")
+    print("Using card ending in " + yourUser.getPay())
+    yourCart.commitToFile()
+    yourCart = Cart()
+    postLoginLoop()
     
 #main
-# create instnace of Inventory, Cart, User
+# create instnace of Inventory, booklist
+ourInv = Inventory()
+bookList = []
+# adds books to book list using isbns from inventory
+for x in ourInv.getISBNs():
+    y = Book(x)
+    bookList.append(y)
 print("Welcome to the Group 11 Bookstore. Please login or create an account: \n")
 while(True):
     x, user = preLoginLoop()
     if(x):
+        yourCart = Cart()
+        yourUser = User()
         postLoginLoop(user)
