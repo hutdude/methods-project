bd20d3b06dc06d604c1be913055e439fcb21c67c
TK-1813 <102626060+TK-1813@users.noreply.github.com>
2022-05-01 13:19:18 -0500

    Add files via upload

--- Inventory.py
+++ Inventory.py
@@ -1,34 +1,39 @@
 import os
 
 
+
 class Inventory:
+
     def __init__(self):
         self.isbns = []
-        self.qtys = []
-
-        # Finds files in directory
+        self.qty = []
+        
+        ## Finds file path 
         here = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(here, 'inventory.txt')
-
+        
         # Opens files
-        f = open(filename, "r")
-        file_contents = f.read()
-        lines = file_contents.splitlines()
+        try:
+            f = open("Inventory.txt", "r")
+            f_contents = f.read()
+            lines = f_contents.splitlines()
+                
+            for x in lines:        
+             isbn_content = x.split("<"|">"|",")
+             self.isbns.append(isbn_content)
+             qty_content = int(x)
+             self.qty.append(qty_content)
+            f.close()
+        
+        except IOError:
+            print("File not found")
+            return "-1"
 
-        for x in lines:
-            temp_isbn = x.split(", ")
-            self.isbns.append(temp_isbn)
-            temp_null = x.split(", ")
-            temp_null2 = x.split(", ")
-            temp_null3 = x.split(", ")
-            temp_qty = int(x)
-            self.qtys.append(temp_qty)
-        f.close()
 
-    def get_isbns(self):
+    def getqty(self):
         return self.isbns
 
-    def sub_qty(self, isbn, qty):
+    def edit_qty(self, isbn, qty):
         i = 0
         while i < len(self.isbns):
             if self.isbns[i] == isbn:
@@ -37,18 +42,14 @@ class Inventory:
             else:
                 i += i
 
-    def add_qty(self, isbn, qty):
-        i = 0
-        while i < len(self.isbns):
-            if self.isbns[i] == isbn:
-                self.qtys[i] = int(self.qtys[i]) + int(qty)
-                break
-            else:
-                i += i
-
-    def commit_book_to_file(self, isbn, title, author, genre, qty, price):
+    def create_new_file(self, isbn, title, author, genre, qty, price):
         here = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(here, 'inventory.txt')
-        f = open(filename, "a")
-        f.write(isbn + ", " + title + ", " + author + ", " + genre + ", " + qty + ", " + price)
+        f = open(filename, "New Inventory")
+        f.write(isbn + "," + title + ", " + author + ", " + genre + ", " + qty + ", " + price)
         f.close()
+
+
+
+
+
